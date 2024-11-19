16aa29cfa9565f1d46a8fb1989d862f8ded74671
DavidIncarbone <david.incarbone@gmail.com>
2024-11-19 12:59:16 +0100

    take first six object from API

--- script/script.js
+++ script/script.js
@@ -1,6 +1,14 @@
 import { oddsOrEven } from "./functions.js";
 
+const baseUrl = "https://jsonplaceholder.typicode.com/";
+const resourcePhotos = "photos";
 
+const params = { "_limit": 6 };
+
+
+axios.get(baseUrl + resourcePhotos, { params }).then((res) => {
+    console.log(res.data);
+})
 
 
 
