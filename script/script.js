7a0acc12bc0a670b1a05db5bd173845d71c8ba46
DavidIncarbone <david.incarbone@gmail.com>
2024-11-19 14:07:10 +0100

    add card template and media queries

--- script/script.js
+++ script/script.js
@@ -1,14 +1,23 @@
 import { oddsOrEven } from "./functions.js";
 
+// get elements by index.html
+
+const container = document.querySelector(".container");
+console.log(container);
+
+
 const baseUrl = "https://jsonplaceholder.typicode.com/";
+
 const resourcePhotos = "photos";
+const resourceP = ""
 
 const params = { "_limit": 6 };
 
 
-axios.get(baseUrl + resourcePhotos, { params }).then((res) => {
-    console.log(res.data);
-})
+axios.get(baseUrl + resourcePhotos, { params })
+    .then((res) => {
+        console.log(res.data);
+    })
 
 
 
