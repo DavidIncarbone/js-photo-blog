d2393a16aa4dbed0d0d837eef31112e5b151bd50
DavidIncarbone <david.incarbone@gmail.com>
2024-11-19 15:33:09 +0100

    add cards with inner.html

--- script/script.js
+++ script/script.js
@@ -9,14 +9,38 @@ console.log(container);
 const baseUrl = "https://jsonplaceholder.typicode.com/";
 
 const resourcePhotos = "photos";
-const resourceP = ""
+
+const completeUrl = baseUrl + resourcePhotos;
 
 const params = { "_limit": 6 };
 
 
 axios.get(baseUrl + resourcePhotos, { params })
-    .then((res) => {
-        console.log(res.data);
+    .then((photos) => {
+        const cards = photos.data;
+        console.log(cards);
+        const images = [];
+        let template = "";
+        for (let photo of cards) {
+            const images = photo.url;
+            const title = photo.title;
+            template += `
+                <div class="card debug">
+                <div class="pin">
+                    <img src="./img/pin.svg" alt="pin">
+                </div>
+                <div class="img-container">
+                    <img src="${images}" alt="${title}">
+                </div>
+                <div class="text-container">
+                    <p>${title}</p>
+                </div>
+                </div>`
+        }
+        container.innerHTML = template;
+    })
+    .catch((error) => {
+        console.log(error);
     })
 
 
