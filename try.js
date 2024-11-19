43117bd8578daa371475f1af101f4888337b690a
DavidIncarbone <david.incarbone@gmail.com>
2024-11-19 12:41:35 +0100

    create container in index.html

---
+++ try.js
@@ -0,0 +1,10 @@
+// const baseUrl = "https://jsonplaceholder.typicode.com/";
+// const resource = "photos";
+
+
+// const params = { "_limit": 6 };
+
+
+// axios.get(baseUrl + resource, { params }).then((res) => {
+//     console.log(res.data);
+// })
